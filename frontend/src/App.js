0069408de54584aa4a70e69436083dd17008e3a9
suhas <suhas.salian27@gmail.com>
2020-07-04 18:44:45 +0530

    product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 import data from "./data";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+import {BrowserRouter, Route, Link} from "react-router-dom"
 
 function App() {
     const openmenu = () => {
@@ -10,68 +13,45 @@ function App() {
         document.querySelector(".sidebar").classList.remove("open");
     };
     return (
-        <div className="grid-container">
-            <header className="header">
-                <div className="brand">
-                    <button onClick={openmenu}>&#9776;</button>
-                    <a href="index.html">amazon</a>
-                </div>
-                <div className="header-links">
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">SignIn</a>
-                </div>
-            </header>
-            <aside className="sidebar">
-                <h3>Shopping Categories</h3>
-                <button className="sidebar-close-button" onClick={closemenu}>
-                    x
-                </button>
-                <ul>
-                    <li>
-                        <a href="index.html">Pants</a>
-                    </li>
-                    <li>
-                        <a href="index.html">Shirts</a>
-                    </li>
-                </ul>
-            </aside>
-            <main className="main">
-                <div className="content">
-                    <ul className="products">
-                        {data.products.map(product => 
-                            <li>
-                                <div className="product">
-                                    <img
-                                        className="product-image"
-                                        src={product.image}
-                                        alt="product"
-                                    />
-                                    <div className="product-name">
-                                        <a
-                                            href="product.html"
-                                            className="product-name"
-                                        >
-                                            {product.name}
-                                        </a>
-                                    </div>
-                                    <div className="product-brand">
-                                        {product.brand}
-                                    </div>
-                                    <div className="product-price">
-                                        {product.price}
-                                    </div>
-                                    <div className="product-rating">
-                                        {product.rating} Stars{" "}
-                                        {product.numReviews}
-                                    </div>
-                                </div>
-                            </li>
-                        )}
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="header">
+                    <div className="brand">
+                        <button onClick={openmenu}>&#9776;</button>
+                        <Link to='/'>amazon</Link>
+                    </div>
+                    <div className="header-links">
+                        <a href="cart.html">Cart</a>
+                        <a href="signin.html">SignIn</a>
+                    </div>
+                </header>
+                <aside className="sidebar">
+                    <h3>Shopping Categories</h3>
+                    <button
+                        className="sidebar-close-button"
+                        onClick={closemenu}
+                    >
+                        x
+                    </button>
+                    <ul>
+                        <li>
+                            <a href="index.html">Pants</a>
+                        </li>
+                        <li>
+                            <a href="index.html">Shirts</a>
+                        </li>
                     </ul>
-                </div>
-            </main>
-            <footer className="footer">All right reserved</footer>
-        </div>
+                </aside>
+                <main className="main">
+                    <div className="content">
+                    <Route path="/product/:id" component={ProductScreen}/>
+                    <Route path="/" exact={true} component={HomeScreen}/>
+                        
+                    </div>
+                </main>
+                <footer className="footer">All right reserved</footer>
+            </div>
+        </BrowserRouter>
     );
 }
 
