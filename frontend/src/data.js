49ea2bdaf589e81fc6e387bf284393e85f968109
suhas <suhas.salian27@gmail.com>
2020-07-04 16:18:51 +0530

    Add Sample Products to Data.js

---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+export default {
+    products: [
+        {
+            name: "Nike T-Shirt",
+            category: 'Shirts',
+            image: "/images/d1.jpeg",
+            price: "$60",
+            brand: "Nike",
+            rating: 4.6,
+            numReviews: 234
+        },
+        {
+            name: "Nike Sports T-Shirt",
+            category: 'Shirts',
+            image: "/images/d1.jpeg",
+            price: "$60",
+            brand: "Nike",
+            rating: 4.2,
+            numReviews: 245
+        },
+        {
+            name: "Nike Joggers",
+            category: 'Pants',
+            image: "/images/d1.jpeg",
+            price: 70,
+            brand: "Nike",
+            rating: 4.7,
+            numReviews: 124
+        },
+        {
+            name: "Nike T-Shirt",
+            category: 'Shirts',
+            image: "/images/d1.jpeg",
+            price: "$60",
+            brand: "Nike",
+            rating: 4.6,
+            numReviews: 234
+        },
+        {
+            name: "Nike Sports T-Shirt",
+            category: 'Shirts',
+            image: "/images/d1.jpeg",
+            price: "$60",
+            brand: "Nike",
+            rating: 4.2,
+            numReviews: 245
+        },
+        {
+            name: "Nike Joggers",
+            category: 'Pants',
+            image: "/images/d1.jpeg",
+            price: 70,
+            brand: "Nike",
+            rating: 4.7,
+            numReviews: 124
+        }
+    ]
+};
