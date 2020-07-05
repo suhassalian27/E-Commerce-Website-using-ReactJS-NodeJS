ad40206b6f9bc0ab2d35ca6adfa5d3a584da5945
suhas <suhas.salian27@gmail.com>
2020-07-05 21:47:22 +0530

    add hooks | use server for data

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,12 +1,25 @@
-import React from "react";
-import data from "../data";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function HomeScreen(props) {
+    const [ products, setProduct ] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios.get("/api/products");
+            setProduct(data);
+        }
+        fetchData();
+        return () => {
+            //
+        };
+    }, [])
+
     return (
         <ul className="products">
-            {data.products.map(product => (
-                <li>
+            {products.map(product => (
+                <li key={product._id}>
                     <div className="product">
                         <Link to={"/product/" + product._id}>
                             <img
