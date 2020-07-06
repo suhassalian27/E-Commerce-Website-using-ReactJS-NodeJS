54fbc2b05bbaed3c987ee060cccb5b6b82f61c67
suhas <suhas.salian27@gmail.com>
2020-07-06 22:40:44 +0530

    ProductReducers.js Add Product details Reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,11 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants";
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
+} from "../constants/productConstants";
 
 function productListReducer(state = { products: [] }, action) {
     switch (action.type) {
@@ -9,7 +16,19 @@ function productListReducer(state = { products: [] }, action) {
         case PRODUCT_LIST_FAIL:
             return { loading: false, error: action.payload };
         default:
-        return state;
+            return state;
     }
 }
-export {productListReducer}
+function productDetailsReducer(state = { product: {} }, action) {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload };
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+export { productListReducer, productDetailsReducer };
