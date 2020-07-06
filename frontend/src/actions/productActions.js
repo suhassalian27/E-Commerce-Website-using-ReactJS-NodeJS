9ac4b091e18fe1bd87906a3f9f712ee7ee9b6c84
suhas <suhas.salian27@gmail.com>
2020-07-06 22:39:24 +0530

    ProductAction.js Add Product details Reducer

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,13 +1,16 @@
 import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from "../constants/productConstants";
 import axios from "axios";
 
-const listProducts = () => async (dispatch) => {
+const listProducts = () => async dispatch => {
     try {
-        dispatch({type:PRODUCT_LIST_REQUEST});
+        dispatch({ type: PRODUCT_LIST_REQUEST });
         const { data } = await axios.get("/api/products");
         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
     } catch (error) {
@@ -15,4 +18,14 @@ const listProducts = () => async (dispatch) => {
     }
 };
 
-export { listProducts };
+const detailsProduct = (productId) => async (dispatch) => {
+    try {
+      dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+      const { data } = await axios.get('/api/products/' + productId);
+      dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
+    }
+  };
+
+export { listProducts, detailsProduct };
