e315d77a24b1301b74e6f469ec62e29d3735dbec
suhas <suhas.salian27@gmail.com>
2020-07-06 22:37:24 +0530

    server.js Add Product details Reducer

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
-import express from 'express';
-import data from './data';
+import express from "express";
+import data from "./data";
 
 const app = express();
 
-app.get('/api/products',(req,res)=>{
-    res.send(data.products);
-})
+app.get("/api/products/:id", (req, res) => {
+    const productId = req.params.id;
+    const product = data.products.find(x => x._id === productId);
+    if (product) res.send(product);
+    else res.status(404).send({ msg: "Product Not Found." });
+});
 
-app.listen(5000,()=>(console.log("server started")))
+app.get("/api/products", (req, res) => {
+    res.send(data.products);
+});
+
+app.listen(5000, () => console.log("server started"));
