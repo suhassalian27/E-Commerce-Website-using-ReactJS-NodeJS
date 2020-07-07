f0f2b1e2ecb78594db437a43620b8b9a48c79b2b
suhas <suhas.salian27@gmail.com>
2020-07-07 22:29:35 +0530

    Add js cookie

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,8 +1,93 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../actions/CartAction";
+import { Link } from "react-router-dom";
 
 function CartScreen(props) {
-    return;
-    <div>Cart CartScreen</div>;
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart;
+    const productId = props.match.params.id;
+    const qty = props.location.search
+        ? Number(props.location.search.split("=")[1])
+        : 1;
+    const dispatch = useDispatch();
+    const removeFromCartHandler = productId => {
+        dispatch(removeFromCart(productId));
+    };
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, []);
+    const checkoutHandler =() =>{
+        props.history.push('/signin?redirect=shipping');
+    }
+
+    return (
+        <div className="cart">
+            <div className="cart-list">
+                <ul className="cart-list-container">
+                    <li>
+                        <h3>Shopping Cart</h3>
+                        <div>Price</div>
+                    </li>
+                    {cartItems.length === 0 ? (
+                        <div>Cart is Empty.</div>
+                    ) : (
+                        cartItems.map(item => (
+                            <li>
+                                <div className="cart-image">
+                                    <img src={item.image} alt="product" />
+                                </div>
+                                <div className="cart-name">
+                                    <div>
+                                        <Link to={"/product/" + item.product}>
+                                            {item.name}
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        Qty:
+                                        <select value={item.qty} onChange={(e)=> dispatch(addToCart(item.product, e.target.value))}>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                        </select>
+                                        <button
+                                            type="button"
+                                            onClick={() =>
+                                                removeFromCartHandler(
+                                                    item.product
+                                                )
+                                            }
+                                            className="Button"
+                                        >
+                                            Delete
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="cart-price">${item.price}</div>
+                            </li>
+                        ))
+                    )}
+                </ul>
+            </div>
+            <div className="cart-action">
+                <h3>
+                    Subtotal 
+                    ({cartItems.reduce((a, c) => a +   c.qty, 0)} items) 
+                    :
+                    ${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+                </h3>
+                <button
+                    className="button primary full-width"
+                    disabled={cartItems.length === 0}
+                    onClick ={checkoutHandler}
+                >
+                    Proceed to Checkout
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default CartScreen;
