0dcd926d72564045944bdc88e185ade4239925a3
suhas <suhas.salian27@gmail.com>
2020-07-08 21:50:24 +0530

    server.js (MONOGODB Setup)

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 import express from "express";
 import data from "./data";
+import config from "./config";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const mongodbURL = config.MONGODB_URL;
+mongoose
+    .connect(mongodbURL, {
+        useNewUrlParser: true
+    })
+    .catch(error => {
+        console.log(error.reason);
+    });
 
 const app = express();
 
