76504911ad88dd75fca3c835de954fbb626999cc
suhas <suhas.salian27@gmail.com>
2020-07-08 22:34:24 +0530

    set user routes

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,16 @@ import data from "./data";
 import config from "./config";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
 
 dotenv.config();
 
 const mongodbURL = config.MONGODB_URL;
 mongoose
     .connect(mongodbURL, {
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        // useUnifiedTopology: true
+        useCreateIndex: true
     })
     .catch(error => {
         console.log(error.reason);
@@ -17,6 +20,7 @@ mongoose
 
 const app = express();
 
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
     const product = data.products.find(x => x._id === productId);
@@ -28,4 +32,8 @@ app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
-app.listen(5000, () => console.log("server started"));
+app.listen(5000, () =>
+    console.log(
+        "************************************************** \n The Server has started at : http://localhost:5000"
+    )
+);
