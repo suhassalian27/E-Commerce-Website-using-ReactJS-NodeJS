c5c2c68582e94d63fcb17c656cdcad68e3973b3d
suhas <suhas.salian27@gmail.com>
2020-07-08 22:33:44 +0530

    update usermodel

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, dropDups: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false }
 });
