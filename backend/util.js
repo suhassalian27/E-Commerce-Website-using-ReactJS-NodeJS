293a1dbb26bc2f580f7f6c3fd345e72751e27529
suhas <suhas.salian27@gmail.com>
2020-07-09 22:27:16 +0530

    Update util.js

--- backend/util.js
+++ backend/util.js
@@ -1,10 +1,18 @@
-import jwt from 'jsonwebtoken';
-import config from 'config';
+import jwt from "jsonwebtoken";
+import config from "./config";
 
-const getToken =(user)=>{
-    return jwt.sign(user, config.JWT_SECRET,{
-        expiresIn: '48h'
-    })
-
-
-export {getToken}
+const getToken = (user) => {
+    return jwt.sign(
+        {
+            _id: user._id,
+            name: user.email,
+            email: user.email,
+            isAdmin: user.isAdmin
+        },
+        config.JWT_SECRET,
+        {
+            expiresIn: "48h"
+        }
+    );
+};
+export { getToken };
