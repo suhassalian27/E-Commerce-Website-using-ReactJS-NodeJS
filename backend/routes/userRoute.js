e44a78776cfbdbe499c59cf621ef517621398ef3
suhas <suhas.salian27@gmail.com>
2020-07-09 20:28:19 +0530

    Add Signin Route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,6 +3,24 @@ import User from "../models/usermodel";
 
 const router = express.Router();
 
+router.post("/signin", async (req, res) => {
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    if (signinUser) {
+        res.send({
+            _id : signinUser.id,
+            name : signinUser.name,
+            email : signinUser.email,
+            isAdmin : signinUser.isAdmin,
+            token : getToken(user)
+        })
+    } else {
+        res.status(401).send({msg:'Invalid Email or Password.'})
+    }
+});
+
 router.get("/createadmin", async (req, res) => {
     try {
         const user = new User({
