4b303d9a23b31f24fa4681394ecd3e8d8cbd856c
suhas <suhas.salian27@gmail.com>
2020-07-09 20:49:10 +0530

    Add JWT_Secret

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export default{
-    MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost/amazon'
+    MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost/amazon',
+    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
