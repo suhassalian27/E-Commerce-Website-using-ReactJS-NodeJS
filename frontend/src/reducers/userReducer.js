c87bc2809cf08fada43b55867a0715b479b3a875
suhas <suhas.salian27@gmail.com>
2020-07-11 20:55:15 +0530

    Add RegisterReducer

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,7 +1,10 @@
 import {
     USER_SIGNIN_REQUEST,
     USER_SIGNIN_SUCCESS,
-    USER_SIGNIN_FAIL
+    USER_SIGNIN_FAIL,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL
 } from "../constants/userConstants";
 
 function userSigninReducer(state = {}, action) {
@@ -16,4 +19,16 @@ function userSigninReducer(state = {}, action) {
             return state;
     }
 }
-export { userSigninReducer };
+function userRegisterReducer(state = {}, action) {
+    switch (action.type) {
+        case USER_REGISTER_REQUEST:
+            return { loading: true };
+        case USER_REGISTER_SUCCESS:
+            return { loading: false, userInfo: action.payload };
+        case USER_REGISTER_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+export { userSigninReducer, userRegisterReducer };
