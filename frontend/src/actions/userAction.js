6d89006e195414c005d7c26e45e022220818e6a9
suhas <suhas.salian27@gmail.com>
2020-07-11 20:55:57 +0530

    add UserAction

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -3,7 +3,10 @@ import Cookie from "js-cookie";
 import {
     USER_SIGNIN_REQUEST,
     USER_SIGNIN_SUCCESS,
-    USER_SIGNIN_FAIL
+    USER_SIGNIN_FAIL,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_REQUEST
 } from "../constants/userConstants";
 
 const signin = (email, password) => async dispatch => {
@@ -19,5 +22,19 @@ const signin = (email, password) => async dispatch => {
         dispatch({ type: USER_SIGNIN_FAIL, payload: error.message });
     }
 };
+const register = (name, email, password) => async dispatch => {
+    dispatch({ type: USER_REGISTER_REQUEST, payload: {name, email, password } });
+    try {
+        const { data } = await Axios.post("/api/users/register", {
+            name, 
+            email,
+            password
+        });
+        dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+        Cookie.set("userInfo", JSON.stringify(data));
+    } catch (error) {
+        dispatch({ type: USER_REGISTER_FAIL, payload: error.message });
+    }
+};
 
-export { signin };
+export { signin, register };
