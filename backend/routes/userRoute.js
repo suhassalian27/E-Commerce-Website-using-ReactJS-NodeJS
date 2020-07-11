3019cb1766a9a2e04c917d1c2512d3fb97333c10
suhas <suhas.salian27@gmail.com>
2020-07-11 20:54:27 +0530

    Add Register Route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -21,6 +21,25 @@ router.post("/signin", async (req, res) => {
         res.status(401).send({ message: "Invalid Email or Password." });
     }
 });
+router.post("/register", async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    const newUser = await user.save();
+    if (newUser) {
+        res.send({
+            _id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+            token: getToken(newUser)
+        });
+    } else {
+        res.status(401).send({ message: "Invalid Userdata." });
+    }
+});
 
 router.get("/createadmin", async (req, res) => {
     try {
