5cea7ed471aa0d31481ffa044d76bacad6da01a6
suhas <suhas.salian27@gmail.com>
2020-07-12 17:00:04 +0530

    add isAuth and isAdmin

--- backend/util.js
+++ backend/util.js
@@ -14,4 +14,27 @@ const getToken = user => {
         }
     );
 };
-export { getToken };
+
+const isAuth = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        const onlyToken = token.slice(7, token.length);
+        jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+            if (err) {
+                return res.status(401).send({ msg: "Invalid Token" });
+            }
+            req.user = token;
+            next();
+            return;
+        });
+    }
+    return res.status(401).send({ msg: "Token is not supplied." });
+};
+const isAdmin = (req, res, next) => {
+    if (req.user && re1.user.isAdmin) {
+        return next();
+    }
+    return res.status(401).send({ msg: "Admin Token is not valid" });
+};
+
+export { getToken, isAuth, isAdmin };
