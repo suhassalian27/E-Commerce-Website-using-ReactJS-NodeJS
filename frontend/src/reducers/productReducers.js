16ace171114c79c489eb85e50f7e17338dde91ff
suhas <suhas.salian27@gmail.com>
2020-07-12 16:58:19 +0530

    add productReducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -4,7 +4,10 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_SAVE_REQUEST,
+    PRODUCT_SAVE_SUCCESS,
+    PRODUCT_SAVE_FAIL
 } from "../constants/productConstants";
 
 function productListReducer(state = { products: [] }, action) {
@@ -14,6 +17,18 @@ function productListReducer(state = { products: [] }, action) {
         case PRODUCT_LIST_SUCCESS:
             return { loading: false, products: action.payload };
         case PRODUCT_LIST_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+function productSaveReducer(state = { product: { reviews: [] } }, action) {
+    switch (action.type) {
+        case PRODUCT_SAVE_REQUEST:
+            return { loading: true };
+        case PRODUCT_SAVE_SUCCESS:
+            return { loading: false, success: true, product: action.payload };
+        case PRODUCT_SAVE_FAIL:
             return { loading: false, error: action.payload };
         default:
             return state;
@@ -31,4 +46,4 @@ function productDetailsReducer(state = { product: { reviews: [] } }, action) {
             return state;
     }
 }
-export { productListReducer, productDetailsReducer };
+export { productListReducer, productDetailsReducer, productSaveReducer };
