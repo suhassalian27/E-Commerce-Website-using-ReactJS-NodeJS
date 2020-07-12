a936ae8c717f5c4d67f9e10da92adef50bc5e832
suhas <suhas.salian27@gmail.com>
2020-07-12 16:59:41 +0530

    add productModels

---
+++ backend/models/productModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    price: { type: Number,default:0, required: true },
+    category: { type: String, required: true },
+    countInStocks: { type: Number,default:0, required: true },
+    description: { type: String, required: true },
+    rating: { type: Number,default:0, required: true },
+    numReviews: { type: Number,default:0, required: true },
+    
+});
+
+const productModel = mongoose.model("Product", productSchema);
+
+export default productModel;
