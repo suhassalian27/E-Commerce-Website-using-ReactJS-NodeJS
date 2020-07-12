acc022a98f0f8c695f707c4f11f6db9a917a4c59
suhas <suhas.salian27@gmail.com>
2020-07-12 17:02:34 +0530

    clean code

--- backend/server.js
+++ backend/server.js
@@ -22,16 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
-// app.get("/api/products/:id", (req, res) => {
-//     const productId = req.params.id;
-//     const product = data.products.find(x => x._id === productId);
-//     if (product) res.send(product);
-//     else res.status(404).send({ msg: "Product Not Found." });
-// });
 
-// app.get("/api/products", (req, res) => {
-//     res.send(data.products);
-// });
 
 app.listen(5000, () =>
     console.log(
