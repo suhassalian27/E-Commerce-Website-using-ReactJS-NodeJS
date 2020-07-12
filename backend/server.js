a39b50653981ffacd5bb54618a336ed25036cb17
suhas <suhas.salian27@gmail.com>
2020-07-12 17:00:42 +0530

    clean code,

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import userRoute from "./routes/userRoute";
+import productRoute from "./routes/productRoute";
 
 dotenv.config();
 
@@ -13,22 +14,24 @@ mongoose
     .connect(mongodbUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        useCreateIndex : true
     })
     .catch(error => console.log(error.reason));
 
 const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
-app.get("/api/products/:id", (req, res) => {
-    const productId = req.params.id;
-    const product = data.products.find(x => x._id === productId);
-    if (product) res.send(product);
-    else res.status(404).send({ msg: "Product Not Found." });
-});
+app.use("/api/products", productRoute);
+// app.get("/api/products/:id", (req, res) => {
+//     const productId = req.params.id;
+//     const product = data.products.find(x => x._id === productId);
+//     if (product) res.send(product);
+//     else res.status(404).send({ msg: "Product Not Found." });
+// });
 
-app.get("/api/products", (req, res) => {
-    res.send(data.products);
-});
+// app.get("/api/products", (req, res) => {
+//     res.send(data.products);
+// });
 
 app.listen(5000, () =>
     console.log(
