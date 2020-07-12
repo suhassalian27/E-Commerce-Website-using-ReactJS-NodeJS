76e70c0d0d7c7b00a686ee6b96683f45d6b0de9a
suhas <suhas.salian27@gmail.com>
2020-07-12 16:58:46 +0530

    add productActions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,9 +4,13 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_SAVE_REQUEST,
+    PRODUCT_SAVE_SUCCESS,
+    PRODUCT_SAVE_FAIL
 } from "../constants/productConstants";
 import axios from "axios";
+import Axios from "axios";
 
 const listProducts = () => async dispatch => {
     try {
@@ -18,14 +22,31 @@ const listProducts = () => async dispatch => {
     }
 };
 
-const detailsProduct = (productId) => async (dispatch) => {
+const saveProduct = product => async (dispatch, getState) => {
     try {
-      dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
-      const { data } = await axios.get('/api/products/' + productId);
-      dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+        dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
+        const {
+            userSignin: { userInfo }
+        } = getState();
+        const { data } = await Axios.post("/api/products", product, {
+            headers: {
+                Authorization: "Bearer" + userInfo.token
+            }
+        });
+        dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
     } catch (error) {
-      dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
+      dispatch({ type: PRODUCT_SAVE_FAIL, payload: error.message });
     }
-  };
+};
 
-export { listProducts, detailsProduct };
+const detailsProduct = productId => async dispatch => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+        const { data } = await axios.get("/api/products/" + productId);
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
+    }
+};
+
+export { listProducts, detailsProduct, saveProduct };
