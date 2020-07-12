8f89592e862a384ff0d348720c416b7b15ac8850
suhas <suhas.salian27@gmail.com>
2020-07-12 22:49:48 +0530

    isAuth

--- backend/util.js
+++ backend/util.js
@@ -23,12 +23,13 @@ const isAuth = (req, res, next) => {
             if (err) {
                 return res.status(401).send({ msg: "Invalid Token" });
             }
-            req.user = token;
+            req.user = decode;
             next();
             return;
-        });
+        })
+    }else{
+        return res.status(401).send({ msg: "Token is not supplied." });
     }
-    return res.status(401).send({ msg: "Token is not supplied." });
 };
 const isAdmin = (req, res, next) => {
     if (req.user && re1.user.isAdmin) {
