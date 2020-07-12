bb5c1aa9f030fcb1b6b0f15580cf96c5a3e092c0
suhas <suhas.salian27@gmail.com>
2020-07-12 16:57:34 +0530

    MONGO_DB URL

--- backend/config.js
+++ backend/config.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || "mongodb://127.0.0.1/amazon",
+    MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/amazon",
     JWT_SECRET: process.env.JWT_SECRET || "somethingsecret"
 };
