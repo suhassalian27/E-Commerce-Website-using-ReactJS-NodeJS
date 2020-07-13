e3dbb77ecbbcd6a3f28277358127ea835c7b9298
suhas <suhas.salian27@gmail.com>
2020-07-13 20:11:46 +0530

    fix countInStock

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -6,7 +6,7 @@ const productSchema = new mongoose.Schema({
     brand: { type: String, required: true },
     price: { type: Number,default:0, required: true },
     category: { type: String, required: true },
-    countInStocks: { type: Number,default:0, required: true },
+    countInStock: { type: Number,default:0, required: true },
     description: { type: String, required: true },
     rating: { type: Number,default:0, required: true },
     numReviews: { type: Number,default:0, required: true },
