9aa3984bb295423e1622f502f2d905785060e0aa
suhas <suhas.salian27@gmail.com>
2020-07-13 20:14:23 +0530

    add redirect link

--- frontend/src/Screens/SigninScreen.js
+++ frontend/src/Screens/SigninScreen.js
@@ -9,10 +9,11 @@ function SigninScreen(props) {
     const userSignin = useSelector(state => state.userSignin);
     const { loading, userInfo, error } = userSignin;
     const dispatch = useDispatch();
+    const redirect = props.location.search?props.location.search.split("=")[1]:'/';
 
     useEffect(() => {
         if (userInfo) {
-            props.history.push("/");
+            props.history.push(redirect);
         }
         return () => {
             //
@@ -60,7 +61,7 @@ function SigninScreen(props) {
                     <li>New to Amazon?</li>
                     <li>
                         <Link
-                            to="/register"
+                            to={redirect === "/" ? "register" : "register?redirect="+redirect }
                             className="button secondary text-center"
                         >
                             Create your Amazon account.
