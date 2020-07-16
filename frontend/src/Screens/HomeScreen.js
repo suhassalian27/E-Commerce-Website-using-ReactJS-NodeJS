4ffbf0752a3e335c81ebb16fa07de98ea87c0735
suhas <suhas.salian27@gmail.com>
2020-07-16 20:14:02 +0530

    add courousel component to home

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 
+import Corousel from "../components/Corousel";
+
 function HomeScreen(props) {
     const productList = useSelector(state => state.productList);
     const { products, loading, error } = productList;
@@ -15,36 +17,37 @@ function HomeScreen(props) {
         };
     }, []);
 
-    return loading ? (
-        <div>loading...</div>
-    ) : error ? (
-        <div>{error}</div>
-    ) : (
-        <ul className="products">
-            {products.map(product => (
-                <li key={product._id}>
-                    <div className="product">
-                        <Link to={"/product/" + product._id}>
-                            <img
-                                className="product-image"
-                                src={product.image}
-                                alt="product"
-                            />
-                        </Link>
-                        <div className="product-name">
+    return (
+        <div>
+            <Corousel />
+            {loading ? (<div>loading...</div>) : error ? (<div>{error}</div>) : (
+            <ul className="products">
+                {products.map(product => (
+                    <li key={product._id}>
+                        <div className="product">
                             <Link to={"/product/" + product._id}>
-                                {product.name}
+                                <img
+                                    className="product-image"
+                                    src={product.image}
+                                    alt="product"
+                                />
                             </Link>
+                            <div className="product-name">
+                                <Link to={"/product/" + product._id}>
+                                    {product.name}
+                                </Link>
+                            </div>
+                            <div className="product-brand">{product.brand}</div>
+                            <div className="product-price">{product.price}</div>
+                            <div className="product-rating">
+                                {product.rating} Stars {product.numReviews}
+                            </div>
                         </div>
-                        <div className="product-brand">{product.brand}</div>
-                        <div className="product-price">{product.price}</div>
-                        <div className="product-rating">
-                            {product.rating} Stars {product.numReviews}
-                        </div>
-                    </div>
-                </li>
-            ))}
-        </ul>
+                    </li>
+                ))}
+            </ul>
+            )}
+        </div>
     );
 }
 
