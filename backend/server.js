8a6feca39af0a8c4fd04b443f95a2e71adc5a623
suhas <suhas.salian27@gmail.com>
2020-07-19 21:16:33 +0530

    add PORT

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,13 @@ mongoose
     .catch(error => console.log(error.reason));
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 
-app.listen(5000, () =>
+app.listen(PORT, () =>
     console.log(
         "************************************************** \n The Server has started at : http://localhost:5000"
     )
