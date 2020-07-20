4883cd47efbb41be7db72f509a0451bb67c11267
suhas <suhas.salian27@gmail.com>
2020-07-20 21:25:25 +0530

    v.1

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import bodyParser from "body-parser";
 import userRoute from "./routes/userRoute";
 import productRoute from "./routes/productRoute";
 
+const path = require("path");
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -24,6 +26,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+
+// Serve static assests if in production
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(PORT, () =>
     console.log(
